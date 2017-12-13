bf21f7792f0c129360b46d5850e2c31ff2543c84
ahmedihafez <ah.hafez@gmail.com>
2017-12-13 10:20:13 +0100

    Process CGD chromosomal_features.tab in CgdIdentifiersConverter

--- bio/sources/cgd-identifiers/main/src/org/intermine/bio/dataconversion/CgdIdentifiersConverter.java
+++ bio/sources/cgd-identifiers/main/src/org/intermine/bio/dataconversion/CgdIdentifiersConverter.java
@@ -11,9 +11,12 @@ package org.intermine.bio.dataconversion;
  */
 
 import java.io.Reader;
+import java.util.Iterator;
 
+import org.apache.commons.lang.StringUtils;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
+import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
 
 
@@ -24,8 +27,8 @@ import org.intermine.xml.full.Item;
 public class CgdIdentifiersConverter extends BioFileConverter
 {
     //
-    private static final String DATASET_TITLE = "Add DataSet.title here";
-    private static final String DATA_SOURCE_NAME = "Add DataSource.name here";
+    private static final String DATASET_TITLE = "CGD";
+    private static final String DATA_SOURCE_NAME = "CGD Genes";
 
     /**
      * Constructor
@@ -42,6 +45,63 @@ public class CgdIdentifiersConverter extends BioFileConverter
      * {@inheritDoc}
      */
     public void process(Reader reader) throws Exception {
+        Iterator<?> lineIter = FormattedTextParser.parseTabDelimitedReader(reader);
 
+//      Columns within chromosomal_features.tab:
+//
+//        	1.  Feature name (mandatory); this is the primary systematic name, if available
+//        	2.  Gene name (locus name)
+//        	3.  Aliases (multiples separated by |)
+//        	4.  Feature type
+//        	5.  Chromosome
+//        	6.  Start Coordinate
+//        	7.  Stop Coordinate
+//        	8.  Strand 
+//        	9.  Primary CGDID
+//        	10. Secondary CGDID (if any)
+//        	11. Description
+//        	12. Date Created
+//        	13. Sequence Coordinate Version Date (if any)
+//        	14. Blank
+//        	15. Blank
+//        	16. Date of gene name reservation (if any).
+//        	17. Has the reserved gene name become the standard name? (Y/N)
+//        	18. Name of S. cerevisiae ortholog(s) (multiples separated by |)
+
+        while (lineIter.hasNext()) {
+            String[] line = (String[]) lineIter.next();
+
+            
+            
+            String systematicName = line[0]; // id from the gff file
+            
+            String primaryidentifier = line[8]; // CGDID
+            String secondaryidentifier = line[9]; // CGDID
+            String chromosomeName = line[4];
+            String name = line[1];
+            String shortDesc = line[10]
+            Item gene = createItem("Gene");
+            if ( !StringUtils.isEmpty(chromosomeName) ) 
+            {
+                if (!StringUtils.isEmpty(primaryidentifier)) {
+                    gene.setAttribute("primaryIdentifier", primaryidentifier);
+
+                    if (!StringUtils.isEmpty(systematicName)) {
+                        gene.setAttribute("symbol", systematicName);
+                    }
+                    if (!StringUtils.isEmpty(secondaryidentifier)) {
+                        gene.setAttribute("secondaryidentifier", secondaryidentifier);
+                    }
+                    if (!StringUtils.isEmpty(name)) {
+                        gene.setAttribute("name", name);
+                    }
+                    if (!StringUtils.isEmpty(shortDesc)) {
+                        gene.setAttribute("breifDescription", shortDesc); // brief Description
+                    }
+                }
+                // gene.setReference("organism", getOrganism(YEAST_TAXON));
+                store(gene);
+            }
+        }
     }
 }
