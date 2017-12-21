11b6fac5fe5bd1ba0dc235469d50387260be3d8d
ahmedihafez <ah.hafez@gmail.com>
2017-12-21 12:30:48 +0100

    Add process code to CGOB source, based on HomoloGene source

--- bio/sources/cgob-orthologs/main/src/org/intermine/bio/dataconversion/CgobOrthologsConverter.java
+++ bio/sources/cgob-orthologs/main/src/org/intermine/bio/dataconversion/CgobOrthologsConverter.java
@@ -11,9 +11,20 @@ package org.intermine.bio.dataconversion;
  */
 
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
+import org.apache.commons.collections.keyvalue.MultiKey;
+import org.apache.commons.lang.StringUtils;
+import org.intermine.bio.dataconversion.HomologeneConverter.GeneRecord;
 import org.intermine.dataconversion.ItemWriter;
 import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.FormattedTextParser;
 import org.intermine.xml.full.Item;
 
 
@@ -24,9 +35,29 @@ import org.intermine.xml.full.Item;
 public class CgobOrthologsConverter extends BioFileConverter
 {
     //
-    private static final String DATASET_TITLE = "Add DataSet.title here";
-    private static final String DATA_SOURCE_NAME = "Add DataSource.name here";
+    private static final String DATASET_TITLE = "CGOB Homology";
+    private static final String DATA_SOURCE_NAME = "CGOB";
+    private static final String DEFAULT_IDENTIFIER_TYPE = "secondaryIdentifier";
 
+    
+    
+    
+    
+    
+    
+    private static final String ORTHOLOGUE = "orthologue";
+    private static final String PARALOGUE = "paralogue";
+
+    private static final String EVIDENCE_CODE_ABBR = "AA";
+    private static final String EVIDENCE_CODE_NAME = "Amino acid sequence comparison";
+
+    private static String evidenceRefId = null;
+    
+    
+    private Map<MultiKey, String> identifiersToGenes = new HashMap<MultiKey, String>();
+
+    
+    
     /**
      * Constructor
      * @param writer the ItemWriter used to handle the resultant items
@@ -42,6 +73,156 @@ public class CgobOrthologsConverter extends BioFileConverter
      * {@inheritDoc}
      */
     public void process(Reader reader) throws Exception {
+    	Iterator<?> lineIter = FormattedTextParser.parseTabDelimitedReader(reader);
+    	
+    	// file format
+    	// each clm contains genes from one speices and all othologs genes are in one row
+    	
+    	
+    	// head contains taxonIds of the species
+    	
+    	// current set of genes
+        Set<GeneRecord> genes = null;
 
+    	String[] lineTokens = (String[]) lineIter.next();
+    	
+    	int numberOfSpecies = lineTokens.length;
+    	String[] taxonIds = new String[numberOfSpecies] ;
+    	for(int i = 0 ; i < numberOfSpecies;i++ )
+    	{
+    		taxonIds[i] = lineTokens[i];
+    	}
+    	
+    	while (lineIter.hasNext()) {
+    		
+    		lineTokens = (String[]) lineIter.next();
+            genes = new HashSet<GeneRecord>();
+    		for(int i = 0 ; i < numberOfSpecies;i++ )
+    		{
+    			String geneId = lineTokens[i];
+    			String taxonId = taxonIds[i];
+    			String gene = getGene(geneId, taxonId);
+                genes.add(new GeneRecord(gene, taxonId));
+    		}
+            processHomologues(genes);
+
+    	}
+    }
+    
+    private void processHomologues(Set<GeneRecord> genes)
+            throws ObjectStoreException {
+            Set<GeneRecord> notProcessed = new HashSet<GeneRecord>(genes);
+            for (GeneRecord gene : genes) {
+                notProcessed.remove(gene);
+                for (GeneRecord homologue : notProcessed) {
+                    createHomologue(gene.geneRefId, gene.taxonId, homologue.geneRefId,
+                            homologue.taxonId);
+                    createHomologue(homologue.geneRefId, homologue.taxonId, gene.geneRefId,
+                            gene.taxonId);
+                }
+            }
+    }
+   
+    private void createHomologue(String gene1, String taxonId1, String gene2, String taxonId2)
+            throws ObjectStoreException {
+            Item homologue = createItem("Homologue");
+            homologue.setReference("gene", gene1);
+            homologue.setReference("homologue", gene2);
+            homologue.addToCollection("evidence", getEvidence());
+            homologue.setAttribute("type", taxonId1.equals(taxonId2) ? PARALOGUE : ORTHOLOGUE);
+            store(homologue);
+        }
+    
+    
+    
+    
+    // Not sure if this is correct for cgob dataset
+    private String getEvidence() throws ObjectStoreException {
+        if (evidenceRefId == null) {
+            Item item = createItem("OrthologueEvidenceCode");
+            item.setAttribute("abbreviation", EVIDENCE_CODE_ABBR);
+            item.setAttribute("name", EVIDENCE_CODE_NAME);
+            try {
+                store(item);
+            } catch (ObjectStoreException e) {
+                throw new ObjectStoreException(e);
+            }
+            String refId = item.getIdentifier();
+
+            item = createItem("OrthologueEvidence");
+            item.setReference("evidenceCode", refId);
+            try {
+                store(item);
+            } catch (ObjectStoreException e) {
+                throw new ObjectStoreException(e);
+            }
+
+            evidenceRefId = item.getIdentifier();
+        }
+        return evidenceRefId;
+    }
+    
+    
+    
+    
+    private String getGene(String geneId, String taxonId)
+            throws ObjectStoreException {
+    	
+    	
+    		// TODO :: read and process configuration file
+//            String identifierType = config.get(taxonId);
+//            if (identifierType == null) {
+//                identifierType = DEFAULT_IDENTIFIER_TYPE;
+//            }
+    		// For now use default
+    		String identifierType = DEFAULT_IDENTIFIER_TYPE;
+    	
+    	
+    	
+            // No resolver yet 
+            // Commnet out this section
+//            String resolvedIdentifier = resolveGene(taxonId, ncbiId, symbol);
+//            if (resolvedIdentifier == null) {
+//                return null;
+//            }
+    		
+    		// For now use the id privided it is the secondary id used in the database
+    		
+    		String resolvedIdentifier = geneId;
+    		
+            String refId = identifiersToGenes.get(new MultiKey(taxonId, resolvedIdentifier));
+    		
+            if (refId == null) {
+                Item item = createItem("Gene");
+                if (!StringUtils.isEmpty(identifierType)) {
+                    item.setAttribute(identifierType, resolvedIdentifier);
+                }
+                item.setReference("organism", getOrganism(taxonId));
+                refId = item.getIdentifier();
+                identifiersToGenes.put(new MultiKey(taxonId, resolvedIdentifier), refId);
+                store(item);
+            }
+            return refId;
+        }
+    
+    
+    
+    
+    /**
+     * represents a gene record in the data files
+     * @author Julie
+     */
+    protected class GeneRecord
+    {
+        protected String geneRefId;
+        protected String taxonId;
+        /**
+         * @param geneRefId the reference number of the gene
+         * @param taxonId taxon ID
+         */
+        public GeneRecord(String geneRefId, String taxonId) {
+            this.geneRefId = geneRefId;
+            this.taxonId = taxonId;
+        }
     }
 }
